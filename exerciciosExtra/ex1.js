1e8f66088c0f51d26cbc01fcc9447b9a56a15667
adeonita <adeonita.sousa@gmail.com>
2020-05-24 19:10:36 -0300

    Exercicios extra 1 e 2

---
+++ exerciciosExtra/ex1.js
@@ -0,0 +1,17 @@
+/**
+ * Exercicio extra 1
+ * 
+ * Podemos dizer que é um número da sorte se o número:
+ * é positivo
+ * é um múltiplo de 2 ou 3
+ * não é 15
+ * 
+ * Escreva a função eNumeroDaSorte que, recebendo um número, diz a se é um número da sorte. 
+ * Lembre-se de que o número deve obedecer às três condições mencionadas. 
+ * Seu desafio adicional será: NÃO use o if.
+ */
+
+
+function eNumeroDaSorte(numero){
+    return numero > 0 && (numero % 2 == 0 || numero % 3 == 0) && numero != 15;
+}
---
+++ exerciciosExtra/ex2.js
@@ -0,0 +1,19 @@
+/**
+ * Exercicio extra 2
+ * 
+ * Agora vamos criar uma função que nos diga se o banco está fechado?
+ * Primeiro de tudo precisamos saber que um banco está fechado quando é 
+ * fim de semana (sábado ou domingo) e não está no horário bancário (9 às 15hs).
+ * 
+ * Defina a função possoIrAoBanco que, receba dois parâmetros, 
+ * o primeiro é diaDaSemana (string) e o segundo horaAtual(numero), 
+ * a função deve retornar true, apenas se o banco estiver aberto
+ */
+
+function possoIrAoBanco(diaDaSemana, horaAtual){
+    return (!(diaDaSemana == 'Sábado' || diaDaSemana == 'Domingo')) && (horaAtual >= 9 && horaAtual <= 15) ;
+}
+
+
+console.log(possoIrAoBanco('Sábado', 11));
+console.log(possoIrAoBanco('Domingo',13));
