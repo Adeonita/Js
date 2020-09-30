332afba7be541cd422a3b0d22b782bbbbd6810b3
adeonita <adeonita.sousa@gmail.com>
2020-09-29 21:59:52 -0300

    feat: constructors functions

--- aula83/index.js
+++ aula83/index.js
@@ -1,1 +1,13 @@
-const person = new person(fistName, lastName);
+function Person(fistName, lastName) {
+    this.fistName = fistName;
+    this.lastName = lastName;
+    this.age = 24;
+
+    this.getDataNascimento = () => {
+        const courrentDate = new Date();
+        return courrentDate.getFullYear() - this.age;
+    }
+}
+
+const person = new Person("Adeonita", "S");
+console.log(person.fistName, person.lastName,person.getDataNascimento());
